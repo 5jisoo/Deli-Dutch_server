78e55338e322bdc28097709009a40df91dce840a
5jisoo <56earls@gmail.com>
2022-12-28 20:39:50 +0900

    [FEAT] controller, service 개발중

--- DeliDutch/src/main/java/com/cokothon/DeliDutch/controller/BoardTogController.java
+++ DeliDutch/src/main/java/com/cokothon/DeliDutch/controller/BoardTogController.java
@@ -1,11 +1,20 @@
 package com.cokothon.DeliDutch.controller;
 
+import com.cokothon.DeliDutch.dto.BoardTogDto;
+import com.cokothon.DeliDutch.service.BoardTogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardTogController {
 
+    private final BoardTogService boardTogService;
+    @PostMapping("/api/v1/boardTog")
+    public long save(@RequestBody BoardTogDto boardTogDto) {
+        return boardTogService.save(boardTogDto);
+    }
 
 }
--- DeliDutch/src/main/java/com/cokothon/DeliDutch/service/BoardTogService.java
+++ DeliDutch/src/main/java/com/cokothon/DeliDutch/service/BoardTogService.java
@@ -1,9 +1,19 @@
 package com.cokothon.DeliDutch.service;
 
+import com.cokothon.DeliDutch.dto.BoardTogDto;
+import com.cokothon.DeliDutch.entity.BoardTog;
+import com.cokothon.DeliDutch.repository.BoardTogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardTogService {
+    private final BoardTogRepository boardTogRepository;
+
+    public long save(BoardTogDto boardTogDto) {
+        return boardTogRepository.save(boardTogDto.toBoardTogEntity()).getId();
+    }
 }
