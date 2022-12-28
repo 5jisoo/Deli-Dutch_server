a33919273fa9007be2d58dccc0a475d5ee3f5687
5jisoo <56earls@gmail.com>
2022-12-28 15:09:28 +0900

    initial commit

---
+++ DeliDutch/src/main/java/com/cokothon/DeliDutch/DeliDutchApplication.java
@@ -0,0 +1,13 @@
+package com.cokothon.DeliDutch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliDutchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliDutchApplication.class, args);
+	}
+
+}
