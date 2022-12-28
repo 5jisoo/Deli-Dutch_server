cf91cddfc96a653bac83a1c0cd419b900c79b884
5jisoo <56earls@gmail.com>
2022-12-28 20:17:10 +0900

    [FEAT] controller, service

--- DeliDutch/src/main/java/com/cokothon/DeliDutch/controller/BoardSepController.java
+++ DeliDutch/src/main/java/com/cokothon/DeliDutch/controller/BoardSepController.java
@@ -1,4 +1,11 @@
 package com.cokothon.DeliDutch.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class BoardSepController {
+
+
 }
