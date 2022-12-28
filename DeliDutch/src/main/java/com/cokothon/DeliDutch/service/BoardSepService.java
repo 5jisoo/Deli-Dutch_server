6566c3369e21c1e5436af41aed9540f26d9d39e4
Juuuic <sn990908a@gmail.com>
2022-12-28 22:42:00 +0900

    [Feat] Create BoardSepController, BoardSepService

--- DeliDutch/src/main/java/com/cokothon/DeliDutch/service/BoardSepService.java
+++ DeliDutch/src/main/java/com/cokothon/DeliDutch/service/BoardSepService.java
@@ -1,6 +1,9 @@
 package com.cokothon.DeliDutch.service;
 
+import com.cokothon.DeliDutch.entity.BoardSep;
+import com.cokothon.DeliDutch.entity.BoardTog;
 import com.cokothon.DeliDutch.repository.BoardSepRepository;
+import com.cokothon.DeliDutch.repository.BoardTogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BoardSepService {
     private final BoardSepRepository boardSepRepository;
+
+    public BoardSep save(BoardSep boardSep) {
+        return boardSepRepository.save(boardSep);
+    }
 }
